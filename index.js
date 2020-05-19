a1995499c686d69850cb31630c20febb06e134b0
Nicodemos234 <nicodemos54324@gmail.com>
2020-05-19 12:43:29 -0300

    shhhhhh

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token = "NzEyMDcyMzE1MDY3ODI2Mjc3.XsP6cQ.Ay8qXo_nJY8RJgaIIyLvULc4rPQ";
+const token = "TOKEN";
 
 client.on('ready', () => {
     console.log('O PAI TA ON!');
