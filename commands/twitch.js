1f14e33b3b267886946abeede5404f1cef409f27
nicodemos234 <nicodemos54324@gmail.com>
2021-07-26 11:57:45 -0300

    twitch command fix

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,7 +1,6 @@
 module.exports = {
-	name: 'twitch',
-	description: 'Meu canal da twitch',
-	execute(message, args) {
-		message.channel.send('Siga meu canal da twitch: https://www.twitch.tv/nicodemos234');
-	},
-};
+    name: "twitch",
+    run: async (client, message, args) => {
+        message.channel.send(`Siga meu canal da twitch: https://www.twitch.tv/nicodemos234`)
+    }
+}
