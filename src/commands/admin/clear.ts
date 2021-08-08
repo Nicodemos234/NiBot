e11f41ca34ab708b9307a02a4012ae235db331bd
nicodemos234 <nicodemos54324@gmail.com>
2021-08-08 10:23:04 -0300

    add clear command

--- src/commands/admin/clear.ts
+++ src/commands/admin/clear.ts
@@ -1,6 +1,16 @@
+import { MyClient } from '../../customTypes'
+import { Message } from 'discord.js'
+
 module.exports = {
     name: "clear",
-    run: async (client, message, args) => {
+    run: async (client: MyClient, message: Message, args: any) => {
         
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            message.channel.send("Você precisar ser administrador para isso :(")
+            return;
+        }
+        await message.channel.send("Deletando mensagems (limite 100)");
+        const allMessages = await message.channel.messages.fetch({ limit: 99 })
+        message.channel.messages.channel.bulkDelete(allMessages)
     }
 }
